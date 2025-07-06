30f3b55870c4f2f2cfba59232a9b5a4b74cf7ad8
Naveen Kumar P <naveenkumarp3052004@gmail.com>
2025-07-07 04:20:54 +0530

    Update index.js

--- task/src/index.js
+++ task/src/index.js
@@ -1,23 +1,16 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // Or './App.css' if that's your main CSS
+import './index.css';
 import App from './App';
-import { GoogleOAuthProvider } from '@react-oauth/google'; // <-- 1. Import this
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// <-- 2. REPLACE 'YOUR_GOOGLE_CLIENT_ID.apps.googleusercontent.com' with your actual Client ID
-// You must get this from your Google Cloud Console project.
-const GOOGLE_CLIENT_ID = '978510048405-df343l6lvnmlfmhkbvunscung0f5tf8g.apps.googleusercontent.com';
+const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
 root.render(
   <React.StrictMode>
-    {/* <-- 3. Wrap your App component with GoogleOAuthProvider */}
     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
       <App />
     </GoogleOAuthProvider>
   </React.StrictMode>
 );
-
-// If you previously had `reportWebVitals();` keep it
